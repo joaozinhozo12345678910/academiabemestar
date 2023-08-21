72f1be4c04e12d6e4a7e74d5ab684b4897099845
joao pedro <js0607615@gmail.com>
2023-08-21 09:37:15 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/tcc/academiabemestar/academiabemestar/AcademiabemestarApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.academiabemestar.academiabemestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiabemestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiabemestarApplication.class, args);
+	}
+
+}
